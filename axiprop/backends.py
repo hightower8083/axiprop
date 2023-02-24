fixed Fresnel FFT2 propagator, added adding fixed fft2 normalization required for Fresnel added plartform-dependent fftshift added use of 2D unwrap (new dependency) updated examples

axiprop/backends.py
@@ -26,6 +26,7 @@ def inv_sqr_on_host(self, M, dtype):
     M = M.astype(dtype)
     return M
 
+
 AVAILABLE_BACKENDS = {}
 
 backend_strings_ordered = ['CU', 'CL', 'AF','NP_MKL', 'NP_FFTW', 'NP']
@@ -57,14 +58,18 @@ class BACKEND_NP():
 
     def make_fft2(self, vec_in, vec_out):
         def fft2(a, b):
-            b = np.fft.fft2(a, norm="ortho")
+            b = np.fft.fft2(a)
             return b
 
         def ifft2(a, b):
-            b = np.fft.ifft2(a, norm="ortho")
+            b = np.fft.ifft2(a)
+            return b
+ 
+        def fftshift(a):
+            b = np.fft.fftshift(a)
             return b
 
-        return fft2, ifft2
+        return fft2, ifft2, fftshift
 
 AVAILABLE_BACKENDS['NP'] = BACKEND_NP
 
@@ -79,7 +84,7 @@ try:
         import pyopencl.clmath as clmath
         from reikna.cluda import ocl_api
         from reikna.linalg import MatrixMul
-        from reikna.fft import FFT
+        from reikna.fft import FFT, FFTShift
 
         name = 'CL'
         ctx = create_some_context() #answers=[1,2])
@@ -130,6 +135,7 @@ try:
 
         def make_fft2(self, vec_in, vec_out):
             fft_reikna = self.FFT(vec_in).compile(self.thrd)
+            fftshift_reikna = self.FFTShift(vec_in).compile(self.thrd)
 
             def fft2(a, b):
                 fft_reikna(b, a, inverse=0)[0].wait()
@@ -139,7 +145,12 @@ try:
                 fft_reikna(b, a, inverse=1)[0].wait()
                 return b
 
-            return fft2, ifft2
+            def fftshift(a):
+                b = a.copy()
+                fftshift_reikna(b, a)[0].wait()
+                return b
+
+            return fft2, ifft2, fftshift
 
     AVAILABLE_BACKENDS['CL'] = BACKEND_CL
 except Exception:
@@ -183,14 +194,19 @@ try:
 
         def make_fft2(self, vec_in, vec_out):
             def fft2(a, b):
-                b = self.cp.fft.fft2(a, norm="ortho")
+                b = self.cp.fft.fft2(a)
                 return b
 
             def ifft2(a, b):
-                b = self.cp.fft.ifft2(a, norm="ortho")
+                b = self.cp.fft.ifft2(a)
                 return b
 
-            return fft2, ifft2
+            def fftshift(a):
+                b = self.cp.fft.fftshift(a)
+                return b
+
+            return fft2, ifft2, fftshift
+            
     AVAILABLE_BACKENDS['CU'] = BACKEND_CU
 except Exception:
     pass
@@ -252,7 +268,14 @@ try:
                 b = self.af.signal.ifft2(a)
                 return b
 
-            return fft2, ifft2
+            def fftshift(a):
+                b = self.to_host(a)
+                b = np.fft.fftshift(b)
+                b = self.bcknd.to_device(b)
+                return b
+
+            return fft2, ifft2, fftshift
+
     AVAILABLE_BACKENDS['AF'] = BACKEND_AF
 except Exception:
     pass
@@ -277,7 +300,11 @@ try:
                 b = self.mklifft2(a)
                 return b
 
-            return fft2, ifft2
+            def fftshift(a):
+                b = np.fft.fftshift(a)
+                return b
+
+            return fft2, ifft2, fftshift
 
     AVAILABLE_BACKENDS['NP_MKL'] = BACKEND_NPMKL
 except Exception:
@@ -309,7 +336,11 @@ try:
                 b = self.ifftw2(a, b)
                 return b
 
-            return fft2, ifft2
+            def fftshift(a):
+                b = np.fft.fftshift(a)
+                return b
+
+            return fft2, ifft2, fftshift
 
     AVAILABLE_BACKENDS['NP_FFTW'] = BACKEND_NPFFTW
 except Exception:

axiprop/common.py
@@ -11,6 +11,7 @@ This file contains common classed of axiprop:
 import numpy as np
 from scipy.special import jn, jn_zeros
 from scipy.interpolate import interp1d, RectBivariateSpline
+from unwrap import unwrap as unwrap2d
 import os
 
 from .backends import AVAILABLE_BACKENDS, backend_strings_ordered
@@ -241,15 +242,15 @@ class CommonTools:
         x_new, y_new = r_new
 
         interp_fu_abs = RectBivariateSpline(
-             x_loc, y_loc, np.abs(u_loc)
+            x_loc, y_loc, np.abs(u_loc)
         )
 
         interp_fu_angl = RectBivariateSpline(
-            x_loc, y_loc, np.unwrap(np.angle(u_loc))
+            x_loc, y_loc, unwrap2d(np.angle(u_loc))
         )
 
-        u_slice_abs = interp_fu_abs.ev(x_new, y_new)
-        u_slice_angl = interp_fu_angl.ev(x_new, y_new)
+        u_slice_abs = interp_fu_abs(x_new, y_new)
+        u_slice_angl = interp_fu_angl(x_new, y_new)
 
         u_slice_new = u_slice_abs * np.exp( 1j * u_slice_angl )
         return u_slice_new

axiprop/lib.py
@@ -377,7 +377,7 @@ class PropagatorFFT2(CommonTools, StepperNonParaxial):
         self.u_ht = self.bcknd.zeros((Nx, Ny), dtype)
         self.u_iht = self.bcknd.zeros((Nx, Ny), dtype)
 
-        self.fft2, self.ifft2 = self.bcknd.make_fft2(self.u_iht, self.u_ht)
+        self.fft2, self.ifft2, fftshift = self.bcknd.make_fft2(self.u_iht, self.u_ht)
 
     def TST(self):
         """
@@ -604,6 +604,7 @@ class PropagatorFFT2Fresnel(CommonTools, StepperFresnel):
         Ly, Ny = y_axis
         self.Nx = Nx
         self.Ny = Ny
+        self.dV = Lx/Nx * Ly/Ny
 
         self.x0, self.y0, self.r, self.r2 = self.init_xy_uniform(x_axis, y_axis)
         x, y, r, r2 = self.init_xy_uniform( (N_pad*Lx, N_pad*Nx),
@@ -624,27 +625,28 @@ class PropagatorFFT2Fresnel(CommonTools, StepperFresnel):
         Ny_ext = self.ky.size
         Nx, Ny = self.r2.shape
         dtype = self.dtype
-
-        self.shape_trns_new = (Nx_ext, Ny_ext)
+        self.shape_trns_new = (Nx, Ny)
 
         self.u_loc = self.bcknd.zeros((Nx, Ny), dtype)
         self.u_iht = self.bcknd.zeros((Nx_ext, Ny_ext), dtype)
         self.u_ht = self.bcknd.zeros((Nx_ext, Ny_ext), dtype)
-        self.fft2, self.ifft2 = self.bcknd.make_fft2(self.u_iht, self.u_ht)
+        self.fft2, self.ifft2, self.fftshift = self.bcknd.make_fft2(self.u_iht, self.u_ht)
 
     def TST(self):
         """
         Forward FFT transform.
         """
         self.u_iht[:] = 0.0
-        ix0, iy0, Nx, Ny = self.ix0, self.iy0, self.Nx, self.Ny
-        self.u_iht[ix0 : ix0 + Nx, iy0 : iy0 + Ny] = self.u_loc
-        self.u_ht = self.fft2(self.u_iht, self.u_ht)
+        ix0, iy0 = self.ix0, self.iy0
+        Nx, Ny = self.Nx, self.Ny
 
-        # while testing
-        self.u_ht = self.bcknd.to_host(self.u_ht)
-        self.u_ht = np.fft.fftshift(self.u_ht)
-        self.u_ht = self.bcknd.to_device(self.u_ht)
+        for ix_loc in range(Nx):
+            ix_glob = ix0 + ix_loc
+            self.u_iht[ix_glob, iy0 : iy0 + Ny] = self.u_loc[ix_loc,:]
+
+        self.u_ht = self.fft2(self.u_iht, self.u_ht)
+        self.u_ht = self.fftshift(self.u_ht)
+        self.u_ht *= self.dV
 
     def get_local_grid(self, dz, ikz):
         kz_loc = self.kz[ikz]
@@ -655,8 +657,8 @@ class PropagatorFFT2Fresnel(CommonTools, StepperFresnel):
         return r_loc, r2_loc
 
     def check_new_grid(self, dz):
+        ix0, iy0, Nx, Ny = self.ix0, self.iy0, self.Nx, self.Ny
         kz_max = self.kz.max()
-        self.x = dz * self.kx / kz_max
-        self.y = dz * self.ky / kz_max
+        self.x = dz * self.kx[ix0 : ix0 + Nx] / kz_max
+        self.y = dz * self.ky[iy0 : iy0 + Ny] / kz_max
         self.r_new = (self.x, self.y)
-        return