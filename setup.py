9d2bf62ce2fed8566cc3de233ce2f139ce8db448
Igor Andriyash <igor.andriyash@gmail.com>
2023-05-09 13:40:30 +0200

    fixed a bug with description format for setup pypi

--- setup.py
+++ setup.py
@@ -9,11 +9,14 @@ import axiprop
 # Obtain the long description from README.md
 # If possible, use pypandoc to convert the README from Markdown
 # to reStructuredText, as this is the only supported format on PyPI
-try:
-    import pypandoc
-    long_description = pypandoc.convert( './README.md', 'rst')
-except (ImportError, RuntimeError):
-    long_description = open('./README.md').read()
+with open("./README.md", encoding="utf-8") as f:
+     long_description = f.read()
+
+#try:
+#    import pypandoc
+#    long_description = pypandoc.convert( './README.md', 'rst')
+#except (ImportError, RuntimeError):
+#    long_description = open('./README.md').read()
 
 # Get the package requirements from the requirements.txt file
 with open('requirements.txt') as f:
@@ -25,6 +28,7 @@ setup(
     version=axiprop.__version__,
     description="simple-to-use optical propagation tool",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Igor Andriyash',
     maintainer='Igor Andriyash',
     maintainer_email='igor.andriyash@gmail.com',
