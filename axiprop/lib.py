57c6b79e15cd59a80472b442b1f425eff4ead55c
Igor Andriyash <igor.andriyash@gmail.com>
2023-09-21 14:32:18 +0200

    enabled non-integer value for N_pad for Fresnel propagators set no default padding for FresnelFFT2 propagator

--- axiprop/lib.py
+++ axiprop/lib.py
@@ -442,7 +442,8 @@ class PropagatorResamplingFresnel(CommonTools, StepperFresnel):
         if type(r_axis) is tuple:
             Rmax, Nr = r_axis
             self.Nr = Nr
-            r_axis_ext = ( N_pad * Rmax, N_pad * Nr )
+            Nr_ext_loc = int( np.round( N_pad * Nr  ) )
+            r_axis_ext = ( N_pad * Rmax, Nr_ext_loc )
             self.r_ext, self.Rmax_ext, self.Nr_ext = \
                             self.init_r_uniform(r_axis_ext)
 
@@ -457,9 +458,10 @@ class PropagatorResamplingFresnel(CommonTools, StepperFresnel):
             dr_est = (self.r[1:] - self.r[:-1]).mean()
             Rmax = self.r.max()
             self.Rmax = Rmax + 0.5 * dr_est
+            Nr_ext_add = int( np.round(  Nr*(N_pad-1) ) )
 
             self.r_ext = np.r_[ self.r,
-                Rmax + dr_est * np.arange( 1, Nr*(N_pad-1))
+                Rmax + dr_est * np.arange( 1, Nr_ext_add)
             ]
             self.Rmax_ext = self.r_ext.max() + 0.5 * dr_est
             self.Nr_ext = self.r_ext.size
@@ -469,7 +471,7 @@ class PropagatorResamplingFresnel(CommonTools, StepperFresnel):
         else:
             self.Nkr_new = Nkr_new
             if Nkr_new > Nr * N_pad:
-                warnings.warn(f"Nkr_new>Nr*N_pad={Nr*N_pad} has no effect")
+                warnings.warn(f"Nkr_new>Nr*N_pad={Nr*int(N_pad)} has no effect")
 
         if r_axis_new is None:
             self.Nr_new = self.Nkr_new
@@ -564,7 +566,7 @@ class PropagatorFFT2Fresnel(CommonTools, StepperFresnel):
 
     def __init__(self, x_axis, y_axis, kz_axis,
                  Nx_new=None, Ny_new=None,
-                 N_pad=2, dtype=np.complex128,
+                 N_pad=1, dtype=np.complex128,
                  backend=None, verbose=True):
         """
         Construct the propagator.
@@ -620,8 +622,8 @@ class PropagatorFFT2Fresnel(CommonTools, StepperFresnel):
 
         Lx_ext = N_pad * Lx
         Ly_ext = N_pad * Ly
-        Nx_ext = N_pad * Nx
-        Ny_ext = N_pad * Ny
+        Nx_ext = int( np.round(N_pad * Nx ))
+        Ny_ext = int( np.round( N_pad * Ny ))
 
         self.Nx = Nx
         self.Ny = Ny
