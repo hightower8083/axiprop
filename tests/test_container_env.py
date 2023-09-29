1391652a650ff907e1f4f8fb4a99e4b3624928ad
Igor Andriyash <igor.andriyash@gmail.com>
2023-09-29 22:49:42 +0200

    improve structure of enveloped container tests

--- tests/test_container_env.py
+++ tests/test_container_env.py
@@ -1,28 +1,19 @@
-# -*- coding: utf-8 -*-
-
 import pytest
 import numpy as np
-from scipy.constants import c
 from copy import deepcopy
-
 from axiprop.containers import ScalarFieldEnvelope
 
-LaserEnergy = 1.0e-3
-w0 = 10.e-3
+LaserEnergy = 1.0
+w0 = 10.e-6
 tau = 30e-15
 lambda0 = 0.8e-6
 
 Nt = 128
 k0 = 2 * np.pi / lambda0
-t_axis = np.linspace( -3 * tau, 4 * tau, Nt )
+t_axis = np.linspace( -3.5 * tau, 3.5 * tau, Nt )
 
-@pytest.fixture(scope="function")
-def container():
-    scl_obj = ScalarFieldEnvelope(k0, t_axis)
-    return scl_obj
-
-
-def test_rz(container):
+def gaussian_rt():
+    container = ScalarFieldEnvelope(k0, t_axis)
     Nr = 512
     r_axis = np.linspace( 0.0, 3.5*w0, Nr )
 
@@ -31,81 +22,72 @@ def test_rz(container):
     LaserObject = container.make_gaussian_pulse(
             r_axis, tau, w0, Energy=LaserEnergy, n_ord=2
     )
+    return LaserObject
 
+def gaussian_xyt():
+    Nx = 256
+    x = np.linspace( -3.5*w0, 3.5*w0, Nx )
+    y = np.linspace( -3.5*w0, 3.5*w0, Nx )
+    r = np.sqrt( x[:,None]**2 + y[None,:]**2 )
+
+    container = ScalarFieldEnvelope(k0, t_axis)
+    LaserObject = container.make_gaussian_pulse(
+            (r, x, y), tau, w0, Energy=LaserEnergy, n_ord=2
+    )
+    return LaserObject
+
+def check_energy(LaserObject):
     assert np.allclose(LaserObject.Energy, LaserEnergy, rtol=1e-7)
     assert np.allclose(LaserObject.Energy_ft, LaserEnergy, rtol=1e-2)
+
+def check_waist_rt(LaserObject):
     assert np.allclose(LaserObject.w0, w0, rtol=3e-3)
     assert np.allclose(LaserObject.w0_ft, w0, rtol=3e-3)
 
-    LaserObject_import = container_sup.import_field(
-        LaserObject.Field, r_axis=r_axis
-    )
-
-    assert np.allclose(LaserObject_import.Energy, LaserEnergy, rtol=1e-7)
-    assert np.allclose(LaserObject_import.Energy_ft, LaserEnergy, rtol=1e-2)
-    assert np.allclose(LaserObject_import.w0, w0, rtol=3e-3)
-    assert np.allclose(LaserObject_import.w0_ft, w0, rtol=3e-3)
-
-    LaserObject_import = container_sup.import_field_ft(
-        LaserObject.Field_ft, r_axis=r_axis
-    )
-
-    assert np.allclose(LaserObject_import.Energy, LaserEnergy, rtol=1e-7)
-    assert np.allclose(LaserObject_import.Energy_ft, LaserEnergy, rtol=1e-2)
-    assert np.allclose(LaserObject_import.w0, w0, rtol=3e-3)
-    assert np.allclose(LaserObject_import.w0_ft, w0, rtol=3e-3)
-
-
-def test_3d(container):
-    Nx = 256
-    xaxis = np.linspace( -3.5*w0, 3.5*w0, Nx )
-    yaxis = np.linspace( -3.5*w0, 3.5*w0, Nx )
-
-    r_axis = np.sqrt( xaxis[:,None]**2 + yaxis[None,:]**2 )
-
-    container_sup = deepcopy(container)
-
-    LaserObject = container.make_gaussian_pulse(
-            (r_axis, xaxis, yaxis), tau, w0, Energy=LaserEnergy, n_ord=2
-    )
-
-    assert np.allclose(LaserObject.Energy, LaserEnergy, rtol=1e-7)
-    assert np.allclose(LaserObject.Energy_ft, LaserEnergy, rtol=1e-2)
-
+def check_waist_xyt(LaserObject):
     w0_est = LaserObject.w0
+    w0_est_ft = LaserObject.w0_ft
     assert np.allclose(w0_est[0], w0, rtol=1e-7)
     assert np.allclose(w0_est[1], w0, rtol=1e-7)
+    assert np.allclose(w0_est_ft[0], w0, rtol=1e-7)
+    assert np.allclose(w0_est_ft[1], w0, rtol=1e-7)
 
-    w0_est = LaserObject.w0_ft
-    assert np.allclose(w0_est[0], w0, rtol=1e-7)
-    assert np.allclose(w0_est[1], w0, rtol=1e-7)
+def test_make_gaussian_rz():
+    LaserObject = gaussian_rt()
+    check_energy(LaserObject)
+    check_waist_rt(LaserObject)
 
-    LaserObject_import = container_sup.import_field(
-        LaserObject.Field, r_axis=(r_axis, xaxis, yaxis)
+    container = ScalarFieldEnvelope(k0, t_axis)
+    LaserObject = container.import_field(
+        LaserObject.Field, r_axis=LaserObject.r, make_copy=True
     )
+    check_energy(LaserObject)
+    check_waist_rt(LaserObject)
 
-    assert np.allclose(LaserObject_import.Energy, LaserEnergy, rtol=1e-7)
-    assert np.allclose(LaserObject_import.Energy_ft, LaserEnergy, rtol=1e-2)
-
-    w0_est = LaserObject_import.w0
-    assert np.allclose(w0_est[0], w0, rtol=1e-7)
-    assert np.allclose(w0_est[1], w0, rtol=1e-7)
-
-    w0_est = LaserObject_import.w0_ft
-    assert np.allclose(w0_est[0], w0, rtol=1e-7)
-    assert np.allclose(w0_est[1], w0, rtol=1e-7)
-
-    LaserObject_import = container_sup.import_field_ft(
-        LaserObject.Field_ft, r_axis=(r_axis, xaxis, yaxis)
+    container = ScalarFieldEnvelope(k0, t_axis)
+    LaserObject = container.import_field_ft(
+        LaserObject.Field_ft, r_axis=LaserObject.r, make_copy=True
     )
+    check_energy(LaserObject)
+    check_waist_rt(LaserObject)
 
-    assert np.allclose(LaserObject_import.Energy, LaserEnergy, rtol=1e-7)
-    assert np.allclose(LaserObject_import.Energy_ft, LaserEnergy, rtol=1e-2)
+def test_make_gaussian_xyz():
+    LaserObject = gaussian_xyt()
+    check_energy(LaserObject)
+    check_waist_xyt(LaserObject)
 
-    w0_est = LaserObject_import.w0
-    assert np.allclose(w0_est[0], w0, rtol=1e-7)
-    assert np.allclose(w0_est[1], w0, rtol=1e-7)
+    container = ScalarFieldEnvelope(k0, t_axis)
+    LaserObject = container.import_field(
+        LaserObject.Field, make_copy=True,
+        r_axis=(LaserObject.r, LaserObject.x, LaserObject.y),
+    )
+    check_energy(LaserObject)
+    check_waist_xyt(LaserObject)
 
-    w0_est = LaserObject_import.w0_ft
-    assert np.allclose(w0_est[0], w0, rtol=1e-7)
-    assert np.allclose(w0_est[1], w0, rtol=1e-7)
+    container = ScalarFieldEnvelope(k0, t_axis)
+    LaserObject = container.import_field_ft(
+        LaserObject.Field_ft, make_copy=True,
+        r_axis=(LaserObject.r, LaserObject.x, LaserObject.y),
+    )
+    check_energy(LaserObject)
+    check_waist_xyt(LaserObject)
